e2109c20257d038eb10d6fff1f800ef64f9253e8
Marco Kaul <marcokaul@gmail.com>
2021-04-10 01:17:42 +0200

    Update access node URL

--- src/lib/flow.ts
+++ src/lib/flow.ts
@@ -2,6 +2,6 @@ import { config } from "@onflow/fcl";
 
 export function setup() {
   config()
-    .put("accessNode.api", "http://access.mainnet.nodes.onflow.org:9000")
+    .put("accessNode.api", "https://access-mainnet-beta.onflow.org")
     .put("fcl.eventPollRate", 2000);
 }
